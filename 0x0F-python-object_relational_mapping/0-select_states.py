d68058dee18c1f04dfde8f5a1ad674495b70bc7d
Jojo-Programming <mpagijollyuganda@gmail.com>
2024-03-18 03:23:02 -0700

    testing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,7 @@
 import MySQLdb
 import sys
 
+
 def list_states(username, password, database_name):
     try:
         # Connect to the MySQL server
@@ -17,7 +18,7 @@ def list_states(username, password, database_name):
         )
         # Create a cursor object to interact with the database
         cursor = connection.cursor()
-        # Execute the SQL query to fetch states sorted by states.id in ascending order
+        # Execute the SQL query to fetch states sorted by states.i
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
         # Fetch all the rows as a list of tuples
         states = cursor.fetchall()
@@ -31,6 +32,7 @@ def list_states(username, password, database_name):
     except MySQLdb.Error as e:
         print(f"Error: {e}")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
